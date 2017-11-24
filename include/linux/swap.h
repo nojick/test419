12d4f5dbd1fdbfc328d28626e52fbb98bad4bee2
Matthew Wilcox <willy@infradead.org>
2017-11-24 14:24:59 -0500

    mm: Convert workingset to XArray
    We construct an XA_STATE and use it to delete the node with
    xas_store() rather than adding a special function for this unique
    use case.  Includes a test that simulates this usage for the
    test suite.
    
    Signed-off-by: Matthew Wilcox <willy@infradead.org>

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -304,15 +304,6 @@ void workingset_activation(struct page *page);
 /* Do not use directly, use workingset_lookup_update */
 void workingset_update_node(struct radix_tree_node *node);
 
-/* Returns workingset_update_node() if the mapping has shadow entries. */
-#define workingset_lookup_update(mapping)				\
-({									\
-	radix_tree_update_node_t __helper = workingset_update_node;	\
-	if (dax_mapping(mapping) || shmem_mapping(mapping))		\
-		__helper = NULL;					\
-	__helper;							\
-})
-
 /* linux/mm/page_alloc.c */
 extern unsigned long totalram_pages;
 extern unsigned long totalreserve_pages;
