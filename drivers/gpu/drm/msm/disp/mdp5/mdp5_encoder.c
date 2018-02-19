955d0d5b88c61dccdddd51a34fef711710ef6d35
Rob Clark <robdclark@gmail.com>
2018-02-19 08:17:06 -0500

    drm/msm/mdp5: rework CTL START signal handling
    For DSI cmd-mode and writeback, we need to write the CTL's START
    register to kick things off, but we only want to do that once both
    the encoder and the crtc have a chance to write their corresponding
    flush bits.  The difficulty is that when there is a full modeset
    (ie. encoder state has changed) we want to defer the start until
    encoder->enable().  But if only plane's have changed, we want to do
    this from crtc->commit().
    
    The start_mask was a previous attempt to handle this, but it didn't
    really do the right thing since atomic conversion.
    
    Instead track in the crtc state that the start should be deferred,
    set to try from encoder's (or in future writeback's) atomic_check().
    This way the state is part of the atomic state, and rollback can
    work properly if an atomic test fails.
    
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/disp/mdp5/mdp5_encoder.c
+++ drivers/gpu/drm/msm/disp/mdp5/mdp5_encoder.c
@@ -308,6 +308,7 @@ static int mdp5_encoder_atomic_check(struct drm_encoder *encoder,
 
 	mdp5_cstate->ctl = ctl;
 	mdp5_cstate->pipeline.intf = intf;
+	mdp5_cstate->defer_start = true;
 
 	/*
 	 * This is a bit awkward, but we want to flush the CTL and hit the
