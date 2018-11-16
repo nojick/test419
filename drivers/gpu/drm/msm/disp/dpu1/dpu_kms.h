705b73464c6e83ff81c15cf00d3719da8fa9b7ab
Sean Paul <seanpaul@chromium.org>
2018-11-16 13:42:19 -0500

    drm/msm: dpu: Remove dpu_power_handle
    Now that we don't have any event handlers, remove dpu_power_handle!
    
    Changes in v2:
    - None
    
    Reviewed-by: Jeykumar Sankaran <jsanka@codeaurora.org>
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_kms.h
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_kms.h
@@ -20,7 +20,6 @@
 #include "dpu_hw_top.h"
 #include "dpu_io_util.h"
 #include "dpu_rm.h"
-#include "dpu_power_handle.h"
 #include "dpu_irq.h"
 #include "dpu_core_perf.h"
 
@@ -102,8 +101,6 @@ struct dpu_kms {
 	struct drm_device *dev;
 	int core_rev;
 	struct dpu_mdss_cfg *catalog;
-
-	struct dpu_power_handle phandle;
 
 	/* directory entry for debugfs */
 	struct dentry *debugfs_root;
