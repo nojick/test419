87dc247a45aa98bf7af80a33fab4139386a9a66e
Jonathan Marek <jonathan@marek.ca>
2018-11-21 20:52:35 -0500

    drm/msm: set priv->kms to NULL before uninit
    otherwise, priv->kms is non-NULL and msm_drm_uninit will cause a panic.
    
    Signed-off-by: Jonathan Marek <jonathan@marek.ca>
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -518,6 +518,7 @@ static int msm_drm_init(struct device *dev, struct drm_driver *drv)
 	if (IS_ERR(kms)) {
 		DRM_DEV_ERROR(dev, "failed to load kms\n");
 		ret = PTR_ERR(kms);
+		priv->kms = NULL;
 		goto err_msm_uninit;
 	}
 
