f4b838bcfd0221168deb860dda06f3d3d0290b26
Christoph Hellwig <hch@lst.de>
2019-03-25 15:44:06 +0100

    dma-mapping: add a Kconfig symbol to indicate arch_dma_prep_coherent presence
    Add a Kconfig symbol that indicates an architecture provides a
    arch_dma_prep_coherent implementation, and provide a stub otherwise.
    
    This will allow the generic dma-iommu code to use it while still
    allowing to be built for cache coherent architectures.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Robin Murphy <robin.murphy@arm.com>

--- include/linux/dma-noncoherent.h
+++ include/linux/dma-noncoherent.h
@@ -88,7 +88,13 @@ static inline void arch_sync_dma_for_cpu_all(struct device *dev)
 }
 #endif /* CONFIG_ARCH_HAS_SYNC_DMA_FOR_CPU_ALL */
 
+#ifdef CONFIG_ARCH_HAS_DMA_PREP_COHERENT
 void arch_dma_prep_coherent(struct page *page, size_t size);
+#else
+static inline void arch_dma_prep_coherent(struct page *page, size_t size)
+{
+}
+#endif /* CONFIG_ARCH_HAS_DMA_PREP_COHERENT */
 
 void *uncached_kernel_address(void *addr);
 void *cached_kernel_address(void *addr);
