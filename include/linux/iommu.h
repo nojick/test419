bb270a392ed61d776091f635c5913e7870de1fa0
Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
2019-04-17 19:24:42 +0100

    ACPI/IORT: Check ATS capability in root complex nodes
    Root complex node in IORT has a bit telling whether it supports ATS or
    not. Store this bit in the IOMMU fwspec when setting up a device, so it
    can be accessed later by an IOMMU driver. In the future we'll probably
    want to store this bit at the host bridge or SMMU rather than in each
    endpoint.
    
    Acked-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Signed-off-by: Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -446,9 +446,13 @@ struct iommu_fwspec {
 	const struct iommu_ops	*ops;
 	struct fwnode_handle	*iommu_fwnode;
 	void			*iommu_priv;
+	u32			flags;
 	unsigned int		num_ids;
 	u32			ids[1];
 };
+
+/* ATS is supported */
+#define IOMMU_FWSPEC_PCI_RC_ATS			(1 << 0)
 
 /**
  * struct iommu_sva - handle to a device-mm bond
