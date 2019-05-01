7451555f4013525a6212b3563ad02d7565a52f91
Julien Grall <julien.grall@arm.com>
2019-05-01 14:58:24 +0100

    iommu/dma-iommu: Remove iommu_dma_map_msi_msg()
    A recent change split iommu_dma_map_msi_msg() in two new functions. The
    function was still implemented to avoid modifying all the callers at
    once.
    
    Now that all the callers have been reworked, iommu_dma_map_msi_msg() can
    be removed.
    
    Signed-off-by: Julien Grall <julien.grall@arm.com>
    Reviewed-by: Robin Murphy <robin.murphy@arm.com>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Acked-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- include/linux/dma-iommu.h
+++ include/linux/dma-iommu.h
@@ -73,7 +73,6 @@ int iommu_dma_prepare_msi(struct msi_desc *desc, phys_addr_t msi_addr);
 void iommu_dma_compose_msi_msg(struct msi_desc *desc,
 			       struct msi_msg *msg);
 
-void iommu_dma_map_msi_msg(int irq, struct msi_msg *msg);
 void iommu_dma_get_resv_regions(struct device *dev, struct list_head *list);
 
 #else
@@ -110,10 +109,6 @@ static inline int iommu_dma_prepare_msi(struct msi_desc *desc,
 
 static inline void iommu_dma_compose_msi_msg(struct msi_desc *desc,
 					     struct msi_msg *msg)
-{
-}
-
-static inline void iommu_dma_map_msi_msg(int irq, struct msi_msg *msg)
 {
 }
 
