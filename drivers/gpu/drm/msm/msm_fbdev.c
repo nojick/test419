b3f722dbda7a609f7ca2f96dc83604c9c53a38ca
nojick <nojick@gmail.com>
2024-11-06 11:23:45 +0300

    revert: drm: drm_bridge_attach

--- drivers/gpu/drm/msm/msm_fbdev.c
+++ drivers/gpu/drm/msm/msm_fbdev.c
@@ -164,7 +164,7 @@ struct drm_fb_helper *msm_fbdev_init(struct drm_device *dev)
 
 	drm_fb_helper_prepare(dev, helper, &msm_fb_helper_funcs);
 
-	ret = drm_fb_helper_init(dev, helper);
+	ret = drm_fb_helper_init(dev, helper, priv->num_connectors);
 	if (ret) {
 		DRM_DEV_ERROR(dev->dev, "could not init fbdev: ret=%d\n", ret);
 		goto fail;
