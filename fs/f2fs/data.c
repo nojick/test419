88eeae2ad9f7d9817627c39eb50cbcfa68f79bfa
nojick <nojick@gmail.com>
2024-11-12 13:34:10 +0300

    Revert "pagevec: Use xa_mark_t"
    This reverts commit a2324a48aa643cfd20342bb9306bae496df87527.

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -2003,7 +2003,7 @@ static int f2fs_write_cache_pages(struct address_space *mapping,
 	pgoff_t last_idx = ULONG_MAX;
 	int cycled;
 	int range_whole = 0;
-	xa_mark_t tag;
+	int tag;
 
 	pagevec_init(&pvec);
 
